ea24af3d6b3c2e35f016e0e1027ec3b8af1ea137
Te22e-Grufman-Axel <axel.grufman@elev.ga.ntig.se>
2024-11-07 10:30:41 +0100

    done

---
+++ NäsanöverE2/Program.cs
@@ -0,0 +1,26 @@
+﻿List<string> städer = new List<string>();
+bool exit = false;
+string answer;
+
+while (exit == false)
+{
+    Console.WriteLine("Skriv in en stad:");
+    answer = Console.ReadLine();
+    answer.ToLower();
+    if (answer == "exit") 
+    {
+        exit = true;
+    }
+    else
+    {
+        städer.Add(answer);
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Städer du har skrivit");
+for (int i = 0; i < städer.Count; i++)
+{
+    Console.WriteLine(städer[i]);
+}
+
+Console.ReadLine();
